9ecd15a3f0a57fac49b5621e9578138c623f4bec
nickbastos <nickbastos4@gmail.com>
2020-07-15 22:05:01 -0500

    Updates

---
+++ precache-manifest.9a4ddaf674ba7340e860ffd8a2cdf1a3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e0745a5367d7adef396de76dad7eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc10e50faccf899eeff1",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b7697d77d344228c8157",
+    "url": "/static/css/main.d72579d6.chunk.css"
+  },
+  {
+    "revision": "bc10e50faccf899eeff1",
+    "url": "/static/js/2.ec1141f1.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.ec1141f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7697d77d344228c8157",
+    "url": "/static/js/main.5742c653.chunk.js"
+  },
+  {
+    "revision": "63048c1ac1764d83c5b8",
+    "url": "/static/js/runtime-main.efd1c84e.js"
+  },
+  {
+    "revision": "6c1b7034ec5fff1bd779ec9612f660d3",
+    "url": "/static/media/geoguesser_icon.6c1b7034.svg"
+  },
+  {
+    "revision": "508582abc79be71a163c2fe8322221ae",
+    "url": "/static/media/geoguesser_logo.508582ab.svg"
+  },
+  {
+    "revision": "cdc9a4b8dee78e70b7ba42b2ceec43f8",
+    "url": "/static/media/londonMap.cdc9a4b8.JPG"
+  },
+  {
+    "revision": "08c15e9129c9b7408f4077f17df15747",
+    "url": "/static/media/marker.08c15e91.svg"
+  },
+  {
+    "revision": "c30ee51474cf7ecd85a94774c54ec673",
+    "url": "/static/media/miles.c30ee514.svg"
+  },
+  {
+    "revision": "ec29201ddb99e2d016d5a4c4e6b391a6",
+    "url": "/static/media/score.ec29201d.svg"
+  },
+  {
+    "revision": "e7643ace85cf631cbb64b3f01a581efa",
+    "url": "/static/media/share.e7643ace.svg"
+  }
+]);
